c961c41391ca6cafc609702d105cd75cdd523bde
NekoZX123 <76691550+NekoZX123@users.noreply.github.com>
2025-10-24 14:59:04 +0800

    feat: :bookmark: Prepare config files for v1.2.1 release

--- app/app.js
+++ app/app.js
@@ -1,15 +1,16 @@
-import { app, BrowserWindow, ipcMain, Menu, shell, Tray, session, clipboard } from 'electron';
+import { app, BrowserWindow, ipcMain, Menu, shell, Tray, clipboard } from 'electron';
 import { fileURLToPath } from 'url';
 import { copyFileSync, mkdir } from 'fs';
 import { userInfo } from 'os';
 import { startService, stopService } from './service.js';
 import { startWebSocket } from './webSocket.js';
 import { isFileExist, readLocalFile, writeLocalFile } from './fileManager.js';
+import { deleteCookies, validateCookieExpiration, listenForCookie } from './accountHelper.js';
 
 const __dirname = fileURLToPath(import.meta.url);
 
-const environment = 'dev';
-// const environment = 'build-kyrios-internal';
+// const environment = 'dev';
+const environment = 'build-kyrios-internal';
 let tray;
 let mainWindow = null;
 
@@ -158,187 +159,6 @@ function newWindow(_, title, url, options) {
     newAppWindow.loadURL(url);
 
     return newAppWindow.id;
-}
-
-// 用户账户相关操作
-
-// 获取账号数据位置
-function getAccountDataLocation(platform) {
-    const appData = getAppData();
-    const location = `${appData}/ArcanumMusic_data/accounts/${platform}.arca`;
-    return location;
-}
-// 检查 Cookie 是否过期
-const platformCookies = {
-    'netease': {
-        'url': 'https://music.163.com/',
-        'name': 'MUSIC_U'
-    },
-    'qqmusic': {
-        'url': 'https://qq.com/',
-        'name': 'qm_keyst'
-    },
-    'kuwo': {
-        'url': 'https://www.kuwo.cn/',
-        'name': 'userid'
-    },
-    'kugou': {
-        'url': 'https://kugou.com',
-        'name': 'KuGoo'
-    }
-};
-function deleteCookies(_, platform) {
-    const cookiesFilter = platformCookies[platform];
-    const dataLocation = getAccountDataLocation(platform);
-
-    writeLocalFile(undefined, dataLocation, '');
-    session.defaultSession.cookies.remove(cookiesFilter.url, cookiesFilter.name);
-}
-function validateCookieExpiration(_, platform) {
-    return new Promise((resolve, reject) => {
-        const cookiesFilter = platformCookies[platform];
-        if (!cookiesFilter && platform !== 'all') {
-            reject(`[Error] Invalid platform name ${platform}`);
-        }
-        session.defaultSession.cookies.get({ url: cookiesFilter.url, name: cookiesFilter.name })
-            .then((cookies) => {
-                if (cookies.length === 0) {
-                    console.log(`[Debug] No exising cookies on platform '${platform}'`);
-                    resolve(null);
-                    return;
-                }
-
-                const userToken = cookies[0];
-                const expiration = userToken.expirationDate * 1000; // 转换为毫秒单位
-                // Cookie 已过期 => 删除对应用户数据
-                if (Date.now() > expiration) {
-                    console.log(`[Debug] Cookie data '${cookiesFilter.name}' on platform ${platform} has expired, user data will be cleaned`);
-                    deleteCookies(undefined, platform);
-
-                    resolve(null);
-                    return;
-                }
-
-                // 返回数据
-                resolve(userToken);
-            });
-    });
-}
-
-// 获取网易云用户信息
-function getNeteaseUser(id) {
-    return new Promise((resolve) => {
-        const appWindow = BrowserWindow.fromId(id);
-        if (appWindow) {
-            // 获取用户头像昵称
-            appWindow.webContents.executeJavaScript('window.localStorage.G_USER', true)
-                .then((result) => {
-                    let data = JSON.parse(result);
-
-                    let userData = {
-                        avatarUrl: data['avatarUrl'],
-                        nickname: data['nickname']
-                    };
-
-                    resolve(userData);
-                });
-        }
-    });
-}
-
-// cookie 监听功能
-function listenForCookie(_, id, targets) {
-    return new Promise((resolve) => {
-        const appWindow = BrowserWindow.fromId(id);
-        if (appWindow) {
-            const cookiesMap = {};
-            const checkCompletion = () => {
-                if (targets.every(target => target in cookiesMap)) {
-                    if (targets.includes('MUSIC_U')) { // 网易云音乐
-                        getNeteaseUser(id).then((userData) => {
-                            console.log(`[Debug] User data (netease) received: ${JSON.stringify(userData)}`);
-                            
-                            const result = {
-                                cookies: cookiesMap,
-                                userData: userData
-                            };
-                            
-                            resolve(result);
-                        });
-                    }
-                    else if (targets.includes('uname3')) { // 酷我音乐
-                        const avatarUrl = cookiesMap['pic3'];
-                        const nickname = cookiesMap['uname3'];
-                        const result = {
-                            userData: {
-                                avatarUrl: avatarUrl.replace(/"/g, ''),
-                                nickname: decodeURIComponent(nickname.replace(/%u([\dA-F]{4})/gi, (_, hex) => String.fromCharCode(parseInt(hex, 16)))),
-                            },
-                            cookies: {
-                                userid: cookiesMap['userid']
-                            }
-                        }
-
-                        console.log(`[Debug] User data (kuwo) received: ${JSON.stringify(result)}`);
-
-                        resolve(result);
-                    }
-                    else if (targets.includes('KuGoo')) { // 酷狗音乐
-                        const kugouUser = cookiesMap['KuGoo'];
-                        const userData = kugouUser.split('&');
-                        const formattedUser = {};
-
-                        userData.forEach((data) => {
-                            // 错误处理
-                            if (!data.includes('=')) throw new Error('[Arcanum Music - Error] Invalid data format');
-
-                            const [ key, value ] = data.split('=');
-                            formattedUser[key] = value;
-                        });
-
-                        const nickname = formattedUser['NickName'];
-                        const avatarUrl = formattedUser['Pic'];
-                        const result = {
-                            userData: {
-                                avatarUrl: avatarUrl.replace(/"/g, ''),
-                                nickname: decodeURIComponent(nickname.replace(/%u([\dA-F]{4})/gi, (_, hex) => String.fromCharCode(parseInt(hex, 16)))),
-                            },
-                            cookies: cookiesMap
-                        }
-
-                        console.log(`[Debug] User data (kugou) received: ${JSON.stringify(result)}`);
-
-                        resolve(result);
-                    }
-                    else {
-                        resolve({ 'cookies': cookiesMap });
-                    }
-                }
-            };
-
-            appWindow.webContents.session.cookies.on('changed', (_, cookie) => {
-                if (targets.includes(cookie.name)) {
-                    cookiesMap[cookie.name] = cookie.value;
-                    console.log(`[Debug] ${cookie.name} = ${cookie.value}`);
-                    checkCompletion();
-                }
-            });
-
-            // 起始检查
-            appWindow.webContents.session.cookies.get({})
-                .then((cookies) => {
-                    cookies.forEach(cookie => {
-                        if (targets.includes(cookie.name)) {
-                            cookiesMap[cookie.name] = cookie.value;
-                            console.log(`[Debug] ${cookie.name} = ${cookie.value}`);
-                        }
-                    });
-                    checkCompletion();
-                });
-        } else {
-            resolve({});
-        }
-    });
 }
 
 function minimizeWindow(_) {
